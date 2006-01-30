229b17cb771c0d842a11043b8ef5c9e1590e4d72
russf <russf@localhost>
2006-01-30 23:43:10 +0000

    addressed moved imports
    svn path=/ATContentTypes/branches/photomerge-branch/; revision=18762

--- Products/ATContentTypes/adapters/folder.py
+++ Products/ATContentTypes/adapters/folder.py
@@ -1,6 +1,6 @@
 from Products.ATContentTypes.z3.interfaces import IPhotoAlbum
 from zope.interface import implements
-from Products.ATContentTypes.interfaces import IFilterFolder
+from Products.ATContentTypes.interface import IFilterFolder
 from cStringIO import StringIO
 from zipfile import ZipFile
 from Acquisition import aq_parent
