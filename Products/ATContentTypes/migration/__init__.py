f868fcc8b3c92d8790d8ee69688cb91923971fb1
Hanno Schlichting <hanno@hannosch.eu>
2006-03-09 15:47:09 +0000

    Clean up tag names
    svn path=/ATContentTypes/tags/ATCT-0_2_1-final/; revision=20662

--- Products/ATContentTypes/migration/__init__.py
+++
@@ -1,6 +0,0 @@
-"""AT Content Types migration suite
-"""
-import Products.ATContentTypes.migration.Walker
-import Products.ATContentTypes.migration.Migrator
-import Products.ATContentTypes.migration.ATCTMigrator
-
