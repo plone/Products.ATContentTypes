624934189eb1b03bfa7ced5c78afa6ac47c7b24e
Hanno Schlichting <hanno@hannosch.eu>
2006-06-05 07:09:56 +0000

    Added export handler for smart folder configuration, refined existing handler. Import handler is still missing...
    svn path=/ATContentTypes/branches/plip148-cmf21/; revision=24421

--- Products/ATContentTypes/exportimport/atcttool.py
+++ Products/ATContentTypes/exportimport/atcttool.py
@@ -28,6 +28,12 @@ class ATCTToolXMLAdapter(XMLAdapterBase):
     def _purgeSettings(self):
         self.context.setCMFTypesAreRecataloged()
         self.context.setVersionFromFS()
+        # initialize topic tool
+        self.context.topic_indexes = {}
+        self.context.topic_metadata = {}
+        self.context.allowed_portal_types = []
+        self.context.createInitialIndexes()
+        self.context.createInitialMetadata()
 
     def _initSettings(self, node):
         for child in node.childNodes:
@@ -42,13 +48,41 @@ class ATCTToolXMLAdapter(XMLAdapterBase):
                     self.context.setInstanceVersion(value)
 
     def _extractSettings(self):
+        fragment = self._doc.createDocumentFragment()
         node=self._doc.createElement('cmftypes_are_recataloged')
         node.setAttribute('value', str(bool(self.context.getCMFTypesAreRecataloged())))
-        child=self._doc.createElement('atct_tool_version')
-        child.setAttribute('value', str(self.context.getVersion()))
-        node.appendChild(child)
-        return node
-
+        fragment.appendChild(node)
+        node=self._doc.createElement('atct_tool_version')
+        node.setAttribute('value', str(self.context.getVersion()[1]))
+        fragment.appendChild(node)
+        # topic tool indexes
+        indexes=self._doc.createElement('topic_indexes')
+        for indexname in self.context.getIndexes():
+            index = self.context.getIndex(indexname)
+            child=self._doc.createElement('index')
+            child.setAttribute('name', str(indexname))
+            child.setAttribute('friendlyName', str(index.friendlyName))
+            child.setAttribute('description', str(index.description))
+            child.setAttribute('enabled', str(bool(index.enabled)))
+            for criteria in index.criteria:
+                sub = self._doc.createElement('criteria')
+                sub.appendChild(self._doc.createTextNode(criteria))
+                child.appendChild(sub)
+            indexes.appendChild(child)
+        fragment.appendChild(indexes)
+        # topic tool metadata
+        metadata=self._doc.createElement('topic_metadata')
+        for metaname in self.context.getAllMetadata():
+            meta = self.context.getMetadata(metaname)
+            child=self._doc.createElement('metadata')
+            child.setAttribute('name', str(metaname))
+            child.setAttribute('friendlyName', str(meta.friendlyName))
+            child.setAttribute('description', str(meta.description))
+            child.setAttribute('enabled', str(bool(meta.enabled)))
+            metadata.appendChild(child)
+        fragment.appendChild(metadata)
+        
+        return fragment
 
 def importATCTTool(context):
     """Import ATCT Tool configuration.
