ae97bc0fdc059c774e405122d2f8445cc7117342
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2006-09-15 16:30:27 +0000

    1) this is a quick workaround for a bug with translations, preventing that one can use some criteria type in languages != english. It just skips translations. 2) there was script called that was removed. now we need to call a bound method instead of the script. also it needs a parameter to return a DispalyList. All this is not very clean, but i'am not deep enough in this SmartFolder (and its AJAX usage) topic to commit the cleanest possible solution.
    svn path=/ATContentTypes/branches/1.1/; revision=30441

--- Products/ATContentTypes/skins/ATContentTypes/getXMLSelectVocab.py
+++ Products/ATContentTypes/skins/ATContentTypes/getXMLSelectVocab.py
@@ -5,20 +5,22 @@
 ##bind script=script
 ##bind subpath=traverse_subpath
 ##parameters=method,param,value
-##title=Get a DisplayList and format fot XML request
+##title=Get a DisplayList and format for XML request
 
-params = {param:value}
+params = {param:value, 'display_list': True}
 
 vocab = getattr(context, method)(**params)
 
 RESPONSE = context.REQUEST.RESPONSE
 RESPONSE.setHeader('Content-Type', 'text/xml')
-trans = context.translate
 
-results = [(trans(vocab.getMsgId(item), default=vocab.getValue(item)),item)
-                    for item in vocab]
+# this is broken (for example: language german + field=review_state)
+##results = [(trans(vocab.getMsgId(item), default=vocab.getValue(item)), item)
+##                    for item in vocab]
+
+results = [(vocab.getValue(item), item) for item in vocab]
 
 item_strings = ['^'.join(a) for a in results]
 result_string = '|'.join(item_strings)
 
-return "<div>%s</div>"%result_string
+return "<div>%s</div>" % result_string
