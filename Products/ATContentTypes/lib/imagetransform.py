0d67bc0764194784e9935927d9abf92cac78b003
Encolpe Degoute <encolpe@gmail.com>
2007-09-10 07:51:53 +0000

    Fix indendation to 4 spaces and remove trailing spaces
    svn path=/ATContentTypes/trunk/; revision=48938

--- Products/ATContentTypes/lib/imagetransform.py
+++ Products/ATContentTypes/lib/imagetransform.py
@@ -64,7 +64,7 @@ TRANSPOSE_MAP = {
     ROTATE_180      : _(u'Rotate 180'),
     ROTATE_90       : _(u'Rotate 90 counterclockwise'),
    }
-   
+
 AUTO_ROTATE_MAP = {
     0   : None,
     90  : ROTATE_270,
@@ -76,11 +76,11 @@ AUTO_ROTATE_MAP = {
 
 class ATCTImageTransform(Base):
     """Base class for images containing transformation and exif functions
-    
+
     * exif information
     * image rotation
     """
-    
+
     actions = (
         {
         'id'          : 'transform',
@@ -90,7 +90,7 @@ class ATCTImageTransform(Base):
         'condition'   : 'object/hasPIL',
          },
         )
-    
+
     security = ClassSecurityInfo()
 
     security.declarePrivate('getImageAsFile')
@@ -119,20 +119,20 @@ class ATCTImageTransform(Base):
 
     # image related code like exif and rotation
     # partly based on CMFPhoto
-    
+
     security.declareProtected(View, 'getEXIF')
     def getEXIF(self, img=None, refresh=False):
         """Get the exif informations of the file
-        
+
         The information is cached in _v_image_exif
         """
         cache = '_image_exif'
-        
+
         if refresh:
             setattr(self, cache, None)
-        
+
         exif_data = getattr(self, cache, None)
-        
+
         if exif_data is None or not isinstance(exif_data, dict):
             io = self.getImageAsFile(img, scale=None)
             if io is not None:
@@ -147,7 +147,7 @@ class ATCTImageTransform(Base):
                 # on a file upload which is required later
                 io.seek(0)
                 # remove some unwanted elements lik thumb nails
-                for key in ('JPEGThumbnail', 'TIFFThumbnail', 
+                for key in ('JPEGThumbnail', 'TIFFThumbnail',
                             'MakerNote JPEGThumbnail'):
                     if key in exif_data:
                         del exif_data[key]
@@ -164,7 +164,7 @@ class ATCTImageTransform(Base):
     security.declareProtected(View, 'getEXIFOrientation')
     def getEXIFOrientation(self):
         """Get the rotation and mirror orientation from the EXIF data
-        
+
         Some cameras are storing the informations about rotation and mirror in
         the exif data. It can be used for autorotation.
         """
@@ -172,15 +172,15 @@ class ATCTImageTransform(Base):
         mirror = 0
         rotation = 0
         code = exif.get('Image Orientation', None)
-        
+
         if code is None:
             return (mirror, rotation)
-        
+
         try:
             code = int(code)
         except ValueError:
             return (mirror, rotation)
-            
+
         if code in (2, 4, 5, 7):
             mirror = 1
         if code in (1, 2):
@@ -191,9 +191,9 @@ class ATCTImageTransform(Base):
             rotation = 90
         elif code in (7, 8):
             rotation = 270
-       
+
         return (mirror, rotation)
-    
+
     security.declareProtected(View, 'getEXIFOrigDate')
     def getEXIFOrigDate(self):
         """Get the EXIF DateTimeOriginal from the image (or None)
@@ -217,43 +217,43 @@ class ATCTImageTransform(Base):
             ROTATE_90 (rotate counterclockwise)
             ROTATE_180
             ROTATE_270 (rotate clockwise)
-        """ 
+        """
         method = int(method)
         if method not in TRANSPOSE_MAP:
             raise RuntimeError, "Unknown method %s" % method
-        
+
         target = self.absolute_url() + '/atct_image_transform'
-        
+
         if not HAS_PIL:
             if REQUEST:
                 REQUEST.RESPONSE.redirect(target)
-        
+
         image = self.getImageAsFile()
         image2 = StringIO()
-        
+
         if image is not None:
             img = PIL.Image.open(image)
             del image
             fmt = img.format
             img = img.transpose(method)
             img.save(image2, fmt, quality=zconf.pil_config.quality)
-            
+
             field = self.getField('image')
             mimetype = field.getContentType(self)
             filename = field.getFilename(self)
-            
+
             # because AT tries to get mimetype and filename from a file like
             # object by attribute access I'm passing a string along
             self.setImage(image2.getvalue(), mimetype=mimetype,
                           filename=filename, refresh_exif=False)
-        
+
         if REQUEST:
-             REQUEST.RESPONSE.redirect(target)
+            REQUEST.RESPONSE.redirect(target)
 
     security.declareProtected(ModifyPortalContent, 'autoTransformImage')
     def autoTransformImage(self, REQUEST=None):
         """Auto transform image according to EXIF data
-        
+
         Note: isn't using mirror
         """
         target = self.absolute_url() + '/atct_image_transform'
@@ -264,16 +264,16 @@ class ATCTImageTransform(Base):
             if transform is not None:
                 self.transformImage(transform)
         if REQUEST:
-             REQUEST.RESPONSE.redirect(target)
+            REQUEST.RESPONSE.redirect(target)
         else:
             return mirror, rotation, transform
-             
+
     security.declareProtected(View, 'getTransformMap')
     def getTransformMap(self):
         """Get map for tranforming the image
         """
         return [{'name' : n, 'value' : v} for v, n in TRANSPOSE_MAP.items()]
-    
+
     security.declareProtected(View, 'hasPIL')
     def hasPIL(self):
         """Is PIL installed?
