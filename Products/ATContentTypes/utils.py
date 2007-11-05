cf076efdfac4b541b04dce71cd363ef1f0ed4ff3
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2007-11-05 02:47:46 +0000

    Fixes to ATContentTypes.utils dt2DT and DT2dt, no longer become naive of timezones.  test_bugs includes test that fails on the old code
    svn path=/ATContentTypes/trunk/; revision=53113

--- Products/ATContentTypes/utils.py
+++ Products/ATContentTypes/utils.py
@@ -23,16 +23,53 @@ Some utilities.
 import datetime
 from DateTime import DateTime
 
+class TimeZone(datetime.tzinfo):
+    """Unnamed timezone info"""
+    
+    def __init__(self, minutes):
+        self.minutes = minutes
+    
+    def utcoffset(self, dt):
+        return datetime.timedelta(minutes=self.minutes)
+    
+    def dst(self, dt):
+        return datetime.timedelta(0)
+    
+    def tzname(self):
+        aheadUTC = self.minutes > 0
+        if aheadUTC: 
+            sign = '+'
+            mins = self.minutes * -1
+        else:
+            sign = '-'
+            mins = self.minutes
+        wholehours = int(self.minutes / 60.)
+        minutesleft = self.minutes % 60
+        return """%s%0.2d%0.2d""" % (sign, wholehours, minutesleft)
+
 def dt2DT(date):
     """Convert Python's datetime to Zope's DateTime
     """
-    return DateTime(*date.timetuple()[:6])
+    args = (date.year, date.month, date.day, date.hour, date.minute, date.second, date.microsecond, date.tzinfo)
+    timezone = args[7].utcoffset(date)
+    secs = timezone.seconds
+    days = timezone.days
+    hours = secs/3600 + days*24
+    mod = "+"
+    if hours < 0:
+        mod = ""
+    timezone = "GMT%s%d" % (mod, hours)
+    args = list(args[:6])
+    args.append(timezone)
+    return DateTime(*args)
 
 def DT2dt(date):
     """Convert Zope's DateTime to Pythons's datetime
     """
     # seconds (parts[6]) is a float, so we map to int
     args = map(int, date.parts()[:6])
+    args.append(0)
+    args.append(TimeZone(int(date.tzoffset()/60)))
     return datetime.datetime(*args)
 
 def toTime(date):
