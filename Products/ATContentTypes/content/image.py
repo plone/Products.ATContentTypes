881b1d68b09545a8051cfff6ac5603bb7be3b430
Wichert Akkerman <wichert@wiggy.net>
2008-02-18 13:12:36 +0000

    Remove __bobo_traverse__ from ATNewsItem and ATImage. Instead rely on the generic ImageTraverse publish traverser from Archetypes. This means ATCT trunk now depends on AT trunk
    svn path=/ATContentTypes/trunk/; revision=58905

--- Products/ATContentTypes/content/image.py
+++ Products/ATContentTypes/content/image.py
@@ -172,22 +172,5 @@ class ATImage(ATCTFileContent, ATCTImageTransform):
             self.setTitle(title)
         self.reindexObject()
 
-    def __bobo_traverse__(self, REQUEST, name):
-        """Transparent access to image scales
-        """
-        if name.startswith('image'):
-            field = self.getField('image')
-            image = None
-            if name == 'image':
-                image = field.getScale(self)
-            else:
-                scalename = name[len('image_'):]
-                if scalename in field.getAvailableSizes(self):
-                    image = field.getScale(self, scale=scalename)
-            if image is not None and not isinstance(image, basestring):
-                # image might be None or '' for empty images
-                return image
-
-        return ATCTFileContent.__bobo_traverse__(self, REQUEST, name)
 
 registerATCT(ATImage, PROJECTNAME)
