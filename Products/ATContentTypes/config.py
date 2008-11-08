be7fa415e7ef2a299e1b6fb09c4b7704e49a18ef
Hanno Schlichting <hanno@hannosch.eu>
2008-11-08 23:29:09 +0000

    Forgot to adjust the reindex test. We got rid of some madness.
    svn path=/Products.ATContentTypes/trunk/; revision=75320

--- Products/ATContentTypes/config.py
+++ Products/ATContentTypes/config.py
@@ -1,10 +1,5 @@
 """AT Content Types configuration file
-
-DO NOT CHANGE THIS FILE!
-
-Use ZConfig to configure ATCT
 """
-__docformat__ = 'restructuredtext'
 
 import string
 import os
@@ -14,9 +9,6 @@ from Products.ATContentTypes.configuration import zconf
 ## read http://www.egenix.com/files/python/mxTidy.html for more informations
 MX_TIDY_ENABLED = zconf.mxtidy.enable
 MX_TIDY_OPTIONS= zconf.mxtidy.options
-
-###############################################################################
-## private options
 
 PROJECTNAME = "ATContentTypes"
 TOOLNAME = "portal_atct"
@@ -28,7 +20,7 @@ WWW_DIR = os.path.join(ATCT_DIR, 'www')
 GLOBALS = globals()
 
 ## swallow PIL exceptions when resizing the image?
-SWALLOW_IMAGE_RESIZE_EXCEPTIONS = zconf.swallowImageResizeExceptions.enable
+SWALLOW_IMAGE_RESIZE_EXCEPTIONS = True
 
 ## mxTidy available?
 try:
@@ -47,15 +39,6 @@ MX_TIDY_MIMETYPES = (
     'text/html',
      )
 
-## ExternalStorage available?
-try:
-    from Products.ExternalStorage.ExternalStorage import ExternalStorage
-except ImportError:
-    HAS_EXT_STORAGE = False
-else:
-    HAS_EXT_STORAGE = True
-    del ExternalStorage
-
 ## LinguaPlone addon?
 try:
     from Products.LinguaPlone.public import registerType
@@ -71,13 +54,6 @@ except ImportError:
     HAS_PIL = False
 else:
     HAS_PIL = True
-
-
-## workflow mapping for the installer
-WORKFLOW_DEFAULT  = '(Default)'
-WORKFLOW_FOLDER   = 'folder_workflow'
-WORKFLOW_TOPIC    = 'folder_workflow'
-WORKFLOW_CRITERIA = ''
 
 ## icon map used for overwriting ATFile icons
 ICONMAP = {'application/pdf' : 'pdf_icon.gif',
