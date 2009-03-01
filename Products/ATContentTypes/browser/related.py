8f3abf8233a3f9e0083ac0b97d11a413c210034b
Hanno Schlichting <hanno@hannosch.eu>
2009-03-01 15:28:41 +0000

    Make the related items template more readable
    svn path=/Products.ATContentTypes/trunk/; revision=81525

--- Products/ATContentTypes/browser/related.py
+++ Products/ATContentTypes/browser/related.py
@@ -1,18 +1,39 @@
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from zope.component import getMultiAdapter
+from zope.component import queryUtility
 from zope.publisher.browser import BrowserView
 
 from AccessControl import Unauthorized
+from Products.CMFCore.utils import getToolByName
 
 
 class RelatedItemsView(BrowserView):
 
+    def available(self):
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        return context_state.is_view_template()
+
+    def getInfoFor(self, item):
+        wftool = getToolByName(self.context, 'portal_workflow')
+        return wftool.getInfoFor(item, 'review_state', '')
+
+    def normalizeString(self, text):
+        return queryUtility(IIDNormalizer).normalize(text)
+
+    def use_view_action(self):
+        props = getToolByName(self.context, 'portal_properties')
+        site = props.get('site_properties')
+        return site.get('typesUseViewActionInListings', ())
+
     def computeRelatedItems(self):
-        in_out = self.context.getRelatedItems()
-        mtool = self.context.portal_membership
+        items = self.context.getRelatedItems()
+        mtool = getToolByName(self.context, 'portal_membership')
 
         res = []
-        for d in range(len(in_out)):
+        for d in range(len(items)):
             try:
-                obj = in_out[d]
+                obj = items[d]
             except Unauthorized:
                 continue
             if obj not in res:
