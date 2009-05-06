5e3f5c58de4014f026572a6c288d8b02f1a9f62c
Hanno Schlichting <hanno@hannosch.eu>
2009-05-06 16:26:34 +0000

    Added proper multi-lingual handling to the reference criterion. It should only show referenced content in the same or the neutral language if the uid catalog is language aware.
    svn path=/Products.ATContentTypes/branches/1.3/; revision=85578

--- Products/ATContentTypes/criteria/reference.py
+++ Products/ATContentTypes/criteria/reference.py
@@ -51,7 +51,14 @@ class ATReferenceCriterion(ATSelectionCriterion):
         putils = getToolByName(self, 'plone_utils')
         options = catalog.uniqueValuesFor(self.Field())
 
-        brains = uid_cat(UID=options, sort_on='Title')
+        # If the uid catalog has a Language index restrict the query by it.
+        # We should only shows references to items in the same or the neutral
+        # language.
+        query = dict(UID=options, sort_on='Title')
+        if 'Language' in uid_cat.indexes():
+            query['Language'] = [self.Language(), '']
+
+        brains = uid_cat(**query)
         display = [((putils.pretty_title_or_id(b)).lower(), b.UID, b.Title or b.id) for b in brains]
         display.sort()
         display_list = DisplayList([(d[1], d[2]) for d in display])
