79480e098d642a6c6af47a17515ec1a9e257535a
Vincent Fretin <vincent.fretin@gmail.com>
2009-11-29 05:51:03 +0000

    Revert r103574
    svn path=/Products.ATContentTypes/branches/1.3/; revision=103727

--- Products/ATContentTypes/tests/test_skinScripts.py
+++ Products/ATContentTypes/tests/test_skinScripts.py
@@ -37,8 +37,10 @@ class TestFormatCatalogMetadata(atcttestcase.ATCTSiteTestCase):
         self.script = self.portal.formatCatalogMetadata
 
     def testFormatDate(self):
-        date = 'Nov 02, 2005 01:52 PM'
-        format = "%b %d, %Y %I:%M %p"
+        date = '2005-11-02 13:52:25'
+        format = '%m-%d-%Y %I:%M %p'
+        self.portal.portal_properties.site_properties.manage_changeProperties(
+                            localLongTimeFormat='%m-%d-%Y %I:%M %p')
         self.assertEqual(self.script(date),
                          DateTime(date).strftime(format))
         self.assertEqual(self.script(DateTime(date)),
