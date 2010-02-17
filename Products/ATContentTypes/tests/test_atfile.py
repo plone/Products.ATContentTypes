89d2f7469e27d235c07bce7d975e2ce0a504740f
Hanno Schlichting <hanno@hannosch.eu>
2010-02-17 17:32:28 +0000

    Adjust file tests to match latest plone.app.blob implementation details
    svn path=/Products.ATContentTypes/trunk/; revision=111141

--- Products/ATContentTypes/tests/test_atfile.py
+++ Products/ATContentTypes/tests/test_atfile.py
@@ -93,20 +93,26 @@ class TestSiteATFile(atcttestcase.ATCTTypeTestCase):
     def testUpperCaseFilename(self):
         fakefile = Fakefile()
         fakefile.filename = 'Some-filename-With-Uppercase.txt'
+        normalized = 'some-filename-with-uppercase.txt'
         id = 'file.2005-11-18.4066860573'
         self.folder.invokeFactory(self.portal_type, id)
         self.folder[id].setFile(fakefile)
         self.failIf(id in self.folder)
-        self.failUnless(fakefile.filename in self.folder)
+        self.failUnless(normalized in self.folder)
+        obj = self.folder[normalized]
+        self.assertEquals(fakefile.filename, obj.getFilename())
 
     def testUpperCaseFilenameWithFunnyCharacters(self):
         fakefile = Fakefile()
         fakefile.filename = 'Zope&Plo?ne .txt'
+        normalized = 'zope-plo-ne.txt'
         id = 'file.2005-11-18.4066860574'
         self.folder.invokeFactory(self.portal_type, id)
         self.folder[id].setFile(fakefile)
         self.failIf(id in self.folder)
-        self.failUnless('Zope-Plo-ne .txt' in self.folder)
+        self.failUnless(normalized in self.folder)
+        obj = self.folder[normalized]
+        self.assertEquals(fakefile.filename, obj.getFilename())
 
     def testWindowsUploadFilename(self):
         fakefile = Fakefile()
@@ -152,7 +158,7 @@ class TestATFileFields(atcttestcase.ATCTFieldTestCase):
 
         self.failUnless(ILayerContainer.providedBy(field))
         self.failUnless(field.required == 1, 'Value is %s' % field.required)
-        self.failUnless(field.default == '', 'Value is %s' % str(field.default))
+        self.failUnless(field.default == None, 'Value is %s' % str(field.default))
         self.failUnless(field.searchable == True, 'Value is %s' % field.searchable)
         self.failUnless(field.vocabulary == (),
                         'Value is %s' % str(field.vocabulary))
