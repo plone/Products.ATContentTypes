e43df831247bc1dee5577620c6205b6dd3bbf0a3
Andreas Zeidler <witsch@plone.org>
2010-03-05 13:30:44 +0000

    adjust to the [changeset:34522 switch] to use `ISO8601()` instead of `ISO()`, which doesn't include time zones (refs #10140)
    svn path=/Products.ATContentTypes/trunk/; revision=112353

--- Products/ATContentTypes/skins/ATContentTypes/formatCatalogMetadata.py
+++ Products/ATContentTypes/skins/ATContentTypes/formatCatalogMetadata.py
@@ -14,7 +14,7 @@ if value is None:
     return ''
 
 if same_type(value, DateTime()):
-    return context.toLocalizedTime(value.ISO(), long_format = long_format)
+    return context.toLocalizedTime(value.ISO8601(), long_format = long_format)
 
 # Ugly but fast check for ISO format (ensure we have '-' and positions 4 and 7,
 #  ' ' at positiion 10 and ':' and 13 and 16), then convert just in case.
