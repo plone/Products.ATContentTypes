6f66df36936463ee331c2c8daf4eede57ce7ea68
johannes raggam <raggam-nl@adm.at>
2010-03-22 20:12:35 +0000

    restored necessary setup code. tests pass again.
    svn path=/Products.ATContentTypes/branches/plip9302-vs-event-integration/; revision=113747

--- Products/ATContentTypes/tests/atcttestcase.py
+++ Products/ATContentTypes/tests/atcttestcase.py
@@ -1,9 +1,12 @@
 from Testing import ZopeTestCase
 from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.layer import PloneSiteLayer
 from Products.PloneTestCase.setup import default_user
 from Products.PloneTestCase.setup import default_password
 from Products.PloneTestCase.setup import portal_name
 from Products.PloneTestCase.setup import portal_owner
+ZopeTestCase.installProduct('SiteAccess')
+PloneTestCase.setupPloneSite()
 
 import os
 
@@ -178,6 +181,8 @@ class ATCTTypeTestCase(ATCTSiteTestCase):
 
 class ATCTFieldTestCase(ATCTSiteTestCase, BaseSchemaTest):
     """ ATContentTypes test including AT schema tests """
+
+    layer = PloneSiteLayer
 
     def afterSetUp(self):
         # initalize the portal but not the base schema test
