3dedeace5284d910590fd57d22de03f537fef7c1
Thomas Desvenain <thomas.desvenain@gmail.com>
2010-09-23 12:44:57 +0000

    - Fixed: album view of a collection of Image items   displayed images on three sections (images, folders, others)   due to atctListAlbum script bad use of topic API.
    ATTopic.queryCatalog  and getFolderContents.py methods do not have the same signature, so some kind of code duplication is necessary
    
    svn path=/Products.ATContentTypes/trunk/; revision=144831

--- Products/ATContentTypes/skins/ATContentTypes/atctListAlbum.py
+++ Products/ATContentTypes/skins/ATContentTypes/atctListAlbum.py
@@ -8,17 +8,23 @@
 ##title=Helper method for photo album view
 ##
 result = {}
-
-if context.portal_type == 'Topic':
-    queryMethod = context.queryCatalog
-else:
-    queryMethod = context.getFolderContents
+is_topic = context.portal_type == 'Topic'
 
 if images:
-    result['images'] = queryMethod({'portal_type':('Image',)},full_objects=True)
+    if is_topic:
+        result['images'] = context.queryCatalog(portal_type=('Image',),
+                                                full_objects=True)
+    else:
+        result['images'] = context.getFolderContents({'portal_type': ('Image',)},
+                                                     full_objects=True)
+
 if folders:
     # We don't need the full objects for the folders
-    result['folders'] = queryMethod({'portal_type':('Folder',)})
+    if is_topic:
+        result['folders'] = context.queryCatalog(portal_type='Folder')
+    else:
+        result['folders'] = context.getFolderContents({'portal_type': ('Folder',)})
+
 if subimages:
     # Handle brains or objects
     if getattr(context, 'getPath', None) is not None:
@@ -26,14 +32,23 @@ if subimages:
     else:
         path = '/'.join(context.getPhysicalPath())
     # Explicitly set path to remove default depth
-    result['subimages'] = queryMethod({'portal_type':('Image',), 'path':path})
+    if is_topic:
+        result['subimages'] = context.queryCatalog(portal_type=('Image',),
+                                                   path=path)
+    else:
+        result['subimages'] = getFolderContents({'portal_type': ('Image',),
+                                                 'path': path})
+
 if others:
     searchContentTypes = context.plone_utils.getUserFriendlyTypes()
     filtered = [p_type for p_type in searchContentTypes
-                if p_type not in ('Image', 'Folder',) ]
+                if p_type not in ('Image', 'Folder',)]
     if filtered:
         # We don't need the full objects for the folder_listing
-        result['others'] = queryMethod({'portal_type':filtered})
+        if is_topic:
+            result['others'] = context.queryCatalog(portal_type=filtered)
+        else:
+            result['others'] = getFolderContents({'portal_type': filtered})
     else:
         result['others'] = ()
 
