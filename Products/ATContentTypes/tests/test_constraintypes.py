9b6867896b60cd9d4947fd5bb4add7f2eb774028
Thomas Desvenain <thomas.desvenain@gmail.com>
2011-05-17 11:08:13 +0000

    Fixed: adding a disallowed subobject raises ValueError instead of Unauthorized.
    svn path=/Products.ATContentTypes/trunk/; revision=239454

--- Products/ATContentTypes/tests/test_constraintypes.py
+++ Products/ATContentTypes/tests/test_constraintypes.py
@@ -1,7 +1,6 @@
 from Testing import ZopeTestCase # side effect import. leave it here.
 from Products.ATContentTypes.tests import atcttestcase
 
-from AccessControl import Unauthorized
 from AccessControl.SecurityManagement import newSecurityManager
 
 from Products.ATContentTypes.lib import constraintypes
@@ -35,72 +34,72 @@ class TestConstrainTypes(atcttestcase.ATCTSiteTestCase):
         self.af.setConstrainTypesMode(constraintypes.ENABLED)
         self.af.setLocallyAllowedTypes(['Folder', 'Image'])
         self.af.setImmediatelyAddableTypes(['Folder'])
-        
-        self.failUnlessEqual(self.af.getLocallyAllowedTypes(), 
+
+        self.failUnlessEqual(self.af.getLocallyAllowedTypes(),
                                 ('Folder', 'Image',))
         self.failUnlessEqual(self.af.getImmediatelyAddableTypes(),
                                 ('Folder',))
-        
-        self.assertRaises(Unauthorized, self.af.invokeFactory, 'Document', 'a')
+
+        self.assertRaises(ValueError, self.af.invokeFactory, 'Document', 'a')
         try:
             self.af.invokeFactory('Image', 'image', title="death")
-        except Unauthorized:
+        except ValueError:
             self.fail()
-        
+
     def test_disabled(self):
         self.af.setConstrainTypesMode(constraintypes.DISABLED)
         self.af.setLocallyAllowedTypes(['Folder', 'Image'])
         self.af.setImmediatelyAddableTypes(['Folder'])
-        
+
         # We can still set and persist, even though it is disabled - must
         # remember!
-        self.failUnlessEqual(self.af.getRawLocallyAllowedTypes(), 
+        self.failUnlessEqual(self.af.getRawLocallyAllowedTypes(),
                                 ('Folder', 'Image',))
-        self.failUnlessEqual(self.af.getRawImmediatelyAddableTypes(), 
+        self.failUnlessEqual(self.af.getRawImmediatelyAddableTypes(),
                                 ('Folder',))
-        
+
         try:
             self.af.invokeFactory('Document', 'whatever', title='life')
             self.af.invokeFactory('Image', 'image', title="more life")
-        except Unauthorized:
+        except ValueError:
             self.fail()
-            
+
         # Make sure immediately-addable are all types if we are disabled
         allowedIds = [ctype.getId() for ctype in self.af.allowedContentTypes()]
         self.failUnlessEqual(allowedIds, self.af.getImmediatelyAddableTypes())
-        
+
     def test_acquireFromHomogenousParent(self):
         # Set up outer folder with restrictions enabled
         self.af.setConstrainTypesMode(constraintypes.ENABLED)
         self.af.setLocallyAllowedTypes(['Folder', 'Image'])
         self.af.setImmediatelyAddableTypes(['Folder'])
-        
+
         # Create inner type to acquire (default)
         self.af.invokeFactory('Folder', 'inner', title='inner')
         inner = self.af.inner
-        
+
         inner.setConstrainTypesMode(constraintypes.ACQUIRE)
-        
+
         # Test persistence
         inner.setLocallyAllowedTypes(['Document', 'Event'])
         inner.setImmediatelyAddableTypes(['Document'])
-        
-        self.failUnlessEqual(inner.getRawLocallyAllowedTypes(), 
+
+        self.failUnlessEqual(inner.getRawLocallyAllowedTypes(),
                                 ('Document', 'Event',))
         self.failUnlessEqual(inner.getRawImmediatelyAddableTypes(),
                                 ('Document',))
-        
-        self.assertRaises(Unauthorized, inner.invokeFactory, 'Event', 'a')
+
+        self.assertRaises(ValueError, inner.invokeFactory, 'Event', 'a')
         try:
             inner.invokeFactory('Image', 'whatever', title='life')
-        except Unauthorized:
+        except ValueError:
             self.fail()
-        
+
         # Make sure immediately-addable are inherited
-        self.failUnlessEqual(inner.getImmediatelyAddableTypes(), 
+        self.failUnlessEqual(inner.getImmediatelyAddableTypes(),
                                 self.af.getImmediatelyAddableTypes())
 
-        
+
         # Create a new unprivileged user who can only access the inner folder
         self.portal.acl_users._doAddUser('restricted', 'secret', ['Member'], [])
         inner.manage_addLocalRoles('restricted', ('Manager',))
@@ -110,44 +109,44 @@ class TestConstrainTypes(atcttestcase.ATCTSiteTestCase):
         self.failUnlessEqual(inner.getLocallyAllowedTypes(),
                         ('Folder', 'Image'))
 
-        
+
     def test_acquireFromHetereogenousParent(self):
-    
+
         # Let folder use a restricted set of types
         self.portal.portal_types.Folder.filter_content_types = 1
         self.portal.portal_types.Folder.allowed_content_types = \
             ('Document', 'Image', 'News Item', 'Topic', 'Folder')
-        
+
         # Set up outer folder with restrictions enabled
         self.af.setConstrainTypesMode(constraintypes.ENABLED)
         self.af.setLocallyAllowedTypes(['Folder', 'Image'])
         self.af.setImmediatelyAddableTypes(['Folder', 'Image'])
-  
+
         # Create inner type to acquire (default)
         self.af.invokeFactory('Folder', 'outer', title='outer')
         outer = self.af.outer
-        
+
         outer.invokeFactory('Folder', 'inner', title='inner')
-        inner = outer.inner 
-        
+        inner = outer.inner
+
         inner.setConstrainTypesMode(constraintypes.ACQUIRE)
-        
+
         # Test persistence
         inner.setLocallyAllowedTypes(['Document', 'Event'])
         inner.setImmediatelyAddableTypes(['Document'])
-        
-        self.failUnlessEqual(inner.getRawLocallyAllowedTypes(), 
+
+        self.failUnlessEqual(inner.getRawLocallyAllowedTypes(),
                                 ('Document', 'Event',))
         self.failUnlessEqual(inner.getRawImmediatelyAddableTypes(),
                                 ('Document',))
-        
+
         # Fail - we didn't acquire this, really, since we can't acquire
         # from parent folder of different type
-        self.assertRaises((Unauthorized, ValueError), inner.invokeFactory, 'Topic', 'a')
+        self.assertRaises(ValueError, inner.invokeFactory, 'Topic', 'a')
         self.failIf('News Item' in inner.getLocallyAllowedTypes())
-        
+
         # Make sure immediately-addable are set to default
-        self.failUnlessEqual(inner.getImmediatelyAddableTypes(), 
+        self.failUnlessEqual(inner.getImmediatelyAddableTypes(),
                                 inner.getLocallyAllowedTypes())
 
 
