9c59626073daa97162c2b1d33a39a043f386cd8e
Maurits van Rees <maurits@vanrees.org>
2022-03-16 17:39:26 +0100

    Back to development: 3.0.8
    [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[0] != 2:
     # Prevent creating or installing a distribution with Python 3.
     raise ValueError("Products.ATContentTypes is based on Archetypes, which is Python 2 only.")
 
-version = '3.0.7'
+version = '3.0.8.dev0'
 
 setup(name='Products.ATContentTypes',
       version=version,
